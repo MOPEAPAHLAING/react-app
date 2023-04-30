3988b6de4428aaaddbd3f84e58bc8cae6365166a
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2023-04-30 20:39:13 +0630

    added: button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <div>
+      <Button color="secondary" onClick={() => console.log("Clicked")}>My child</Button>
+    </div>
   );
-}
-
-
-// 1. git init
+};
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useState } from "react";
 
-type Props = {}
-
-const ListGroup = (props: Props) => {
-  return (
-    <div>ListGroup</div>
-  )
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-export default ListGroup
+const ListGroup = ({ items, heading, onSelectItem }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li 
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
